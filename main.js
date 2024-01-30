707beb8179f853d4e1ab9a92d4ac2ebc61a4f642
Jose A. <soigne@Aria.local>
2024-01-30 00:21:12 -0600

    Get coords

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-if (navigator.geolocation){
-  navigator.geolocation.getCurrentPosition(success, error)
+if (navigator.geolocation){ //success
+  navigator.geolocation.getCurrentPosition(
+    (position) => { //success
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(
+        `%clatitude: ${latitude}, longitude: ${longitude}`, 
+        "color: green"
+      )
+    }, 
+    (error) => console.error ( 
+      `%cError getting location: ${error.message}`,
+      "color: red"
+      )
+  )
 } else {
   console.error(
     "%cGeolocation is not supported by this browser.", 
