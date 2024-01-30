afde22d433c01acbdf1c74e5e87064847bd4aaeb
Jose A. <soigne@Aria.local>
2024-01-30 01:33:27 -0600

    Grab json data for forecast api call

--- main.js
+++ main.js
@@ -33,13 +33,37 @@ async function getEndpoints(latitude, longitude){
   try {
     // Use coords to retrieve gridpoints
     const response = await fetch(
-      `https://api.weather.gov/points/${latitude}, ${longitude}`
+      `https://api.weather.gov/points/${latitude},${longitude}`
     )
+    console.log(response)
+
     // If the fetch response fails
     if(!response.ok) {
       throw new Error("Network response was not ok.")
     }
+    
+    // Use get() to view response data type
+    // const contentType = response.headers.get("Content-Type")
+    // console.log(contentType) //result = application/geo+json
+    
+    //Parse response data into readable json format
+    const data = await response.json()
+    console.log(data)
+
+    // We will need the office and points from the data
+    const office = data.properties.gridId
+    const gridX  = data.properties.gridX
+    const gridY  = data.properties.gridY
+
+    // Call the weater API to get the forecast using the data
+    getForecast(office, gridX, gridY)    
+
   } catch(error) {
     console.error(`%c${error.message}`, "color: red")
   }
+}
+
+// Weather.com forecast API
+async function getForecast(office, gridX, gridY){
+
 }
