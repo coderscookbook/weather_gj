d7d18c7aaf98165c23b0c6da940394fe22303a10
Jose A. <soigne@Aria.local>
2024-02-02 07:40:58 -0600

    finished//no errors

--- main.js
+++ main.js
@@ -63,7 +63,68 @@ async function getEndpoints(latitude, longitude){
   }
 }
 
+
 // Weather.com forecast API
 async function getForecast(office, gridX, gridY){
+  try {
+    const response = await fetch(
+      `https://api.weather.gov/gridpoints/${office}/${gridX},${gridY}/forecast`
+    )
+    // If fetch response fails
+    if (!response.ok){
+      throw new Error("Fetching forecast failed")
+    }
+    const forecastData = await response.json()
+    console.log("FORECAST:", forecastData)
 
+    // Store the periods from the data
+    const periods = forecastData.properties.periods
+    // Create the weather cards on the web page
+    createWeatherCards(periods)
+  } catch (error) {
+    console.error(`%c${error.message}`, "color: red")
+  }
+}
+
+function createWeatherCards(periods){
+  // Store the card container div from index.html
+  const cardContainer = document.getElementById("card-container")
+  // Create a card for each element in the periods array
+  periods.forEach((element) => {
+    // Create the html elements
+    const div = document.createElement("div")
+    const h2  = document.createElement("h2")
+    const img = document.createElement("img")
+    const p   = document.createElement("p")
+
+    // Set the icon to be used as the large version
+    const icon = element.icon.replace("medium", "large")
+
+    // Set the new div class to "card"
+    div.setAttribute("class", "card")
+
+    // Set the new img attributes
+    img.setAttribute("src", icon)
+    img.setAttribute("alt", element.shortForecast)
+
+    // Set the text content for the card title and forecast
+    h2.textContent = element.name
+    p.textContent  = element.shortForecast
+
+    // Append the title, image, and forecast to the card div
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(p)
+
+    // Append the card to the card container div
+    cardContainer.appendChild(div)
+
+  })
+  
+  // Set favicon to the first weater icon
+  const head = document.getElementsByTagName("head")[0]
+  const link = document.createElement("link")
+  link.rel   = "icon"
+  link.href  = periods[0].icon.replace("medium", "small")
+  head.appendChild(link)
 }
